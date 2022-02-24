5f966dc032f714369878a6aadf7fe84e50b4e086
SurrealityUwU <chaowanonsrisilpa@gmail.com>
2022-02-24 17:33:29 +0700

    q2

---
+++ q2_paint.py
@@ -0,0 +1,48 @@
+import sys
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+ 
+class Paint(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(100, 100, 800, 600)
+        self.image = QImage(self.size(), QImage.Format_RGB32)
+        self.image.fill(Qt.white)
+        self.last_x, self.last_y = None, None
+
+        self.clearButton = QPushButton("Clear", self)
+        self.clearButton.clicked.connect(self.clear)
+
+    def mousePressEvent(self, event):
+        self.last_x, self.last_y = event.x(), event.y()
+  
+    def mouseMoveEvent(self, event):
+        painter = QPainter(self.image)
+        painter.drawLine(self.last_x, self.last_y, event.x(), event.y())
+
+        self.last_x, self.last_y = event.x(), event.y()
+        self.update()
+ 
+    def mouseReleaseEvent(self, event):
+        self.last_x, self.last_y = None, None
+
+    def paintEvent(self, event):
+        canvasPainter = QPainter(self)
+        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
+ 
+    def clear(self):
+        self.image.fill(Qt.white)
+        self.update()
+
+def main():
+    app = QApplication(sys.argv)
+ 
+    w = Paint()
+    w.show()
+
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())
